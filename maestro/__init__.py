da750d89df6000a2eb32d4d7a6434492fde64ed1
Marshall Warners <m@marshallwarners.com>
2026-07-18 13:54:42 -0400

    Export get_config, HomeAssistantClient, and EntityData from public packages

--- maestro/__init__.py
+++ maestro/__init__.py
@@ -7,10 +7,12 @@ __version__ = version("hass-maestro")
 db = SQLAlchemy()
 
 from maestro.app import MaestroApp, get_app  # noqa: E402
+from maestro.config import get_config  # noqa: E402
 
 __all__ = [
     MaestroApp.__name__,
     get_app.__name__,
+    get_config.__name__,
     "db",
     "__version__",
 ]
--- maestro/integrations/__init__.py
+++ maestro/integrations/__init__.py
@@ -1,6 +1,8 @@
+from .home_assistant.client import HomeAssistantClient
 from .home_assistant.types import (
     AttributeId,
     Domain,
+    EntityData,
     EntityId,
     FiredEvent,
     NotifActionDataT,
@@ -14,8 +16,10 @@ from .state_manager import StateManager
 __all__ = [
     AttributeId.__name__,
     Domain.__name__,
+    EntityData.__name__,
     EntityId.__name__,
     FiredEvent.__name__,
+    HomeAssistantClient.__name__,
     StateChangeEvent.__name__,
     "NotifActionDataT",
     NotifActionEvent.__name__,
